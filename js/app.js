85432cb1f4059c66bb4db0c2ec91e4903173e8a2
mdOmarfaruk151 <mdomar28151@gmail.com>
2026-02-17 22:21:14 +0600

    create base layout with responsive navbar and hero section

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
